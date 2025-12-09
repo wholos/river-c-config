8515a54ead3653d41eea1f0e92e090c8d0f4adc4
holos <holosnikitatt@gmail.com>
2025-12-09 22:11:45 +0200

    Add keymap setup for riverwm in km.c
    Implement keymap configuration for riverwm.

---
+++ km.c
@@ -0,0 +1,18 @@
+// Keymap for riverwm on C
+#include <stdlib.h>
+
+int term = alacritty;
+int menu = wmenu-run;
+
+int main() {
+  system("riverctl map normal Super Z spawn ", term);
+  system("riverctl map normal Super C close");
+  system("riverctl map normal Super R spawn ", menu);
+  system("iverctl map normal Super+Shift Q exit");
+  system("riverctl default-layout rivertile");
+  system("rivertile -view-padding 0 -outer-padding 0 &");
+  system("riverctl map normal Super F toggle-float");
+  system("riverctl spawn \"$HOME/.config/river/status\"");
+  system("riverctl spawn \"$HOME/.config/river/bar\"");
+  system("riverctl keyboard-layout -options \"grp:win_space_toggle\" \"us,ru\"");
+}
