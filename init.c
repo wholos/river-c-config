b7757d1b14b369126aebba60a14342458bb9d66b
holos <holosnikitatt@gmail.com>
2025-12-10 11:19:34 +0200

    Update init.c

--- init.c
+++ init.c
@@ -1,7 +1,18 @@
-// RiverWM C Config
+// RiverWM config on C by holos
 #include <stdlib.h>
 
+int term = alacritty;
+int menu = wmenu-run;
+
 int main() {
-  system("./as");
-  system("./km");
+  system("riverctl map normal Super Z spawn ", term);
+  system("riverctl map normal Super C close");
+  system("riverctl map normal Super R spawn ", menu);
+  system("iverctl map normal Super+Shift Q exit");
+  system("riverctl default-layout rivertile");
+  system("rivertile -view-padding 0 -outer-padding 0 &");
+  system("riverctl map normal Super F toggle-float");
+  system("riverctl spawn \"$HOME/.config/river/status\"");
+  system("riverctl spawn \"$HOME/.config/river/bar\"");
+  system("riverctl keyboard-layout -options \"grp:win_space_toggle\" \"us,ru\"");
 }
